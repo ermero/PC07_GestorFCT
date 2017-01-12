b85dba2a9d1ef775b5339189937d57333928f4f3
ermero <ermero@floridauniversitaria.es>
2017-01-12 18:03:09 +0100

    version final

---
+++ src/FCTBundle/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace FCTBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkBundle\Configuration\Route;
+use FCTBundle\Entity\User;
+use FCTBundle\Form\UserType;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('FCTBundle:Default:index.html.twig');
+    }
+    
+}
